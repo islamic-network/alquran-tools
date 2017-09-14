9771b3b4e9f0f9b68bae73dc40a4fe028c64f702
Meezaan-ud-Din <meezaan@vesica.co.uk>
2017-09-14 16:14:32 +0100

    started creating mapper

---
+++ mapper.php
@@ -0,0 +1,150 @@
+<?php
+
+$map = [
+    'ا' => [
+        'before' => false,
+        'after' => false,
+        'variations' => ['أ', 'إ', 'آ', 'ٱ', 'اَ', 'اِ', 'اُ', 'اْ', 'اً', 'اّ']
+    ],
+    'ب' => [
+        'before' => true,
+        'after' => true,
+        'variations' => ['بَ', 'بِ', 'بُ', 'بٌ', 'بٍ', 'بْ', 'بّ']
+    ],
+    'ت' => [
+        'before' => true,
+        'after' => true,
+        'variations' => ['تَ', 'تِ', 'تُ', 'تٌ', 'تٍ', 'تْ', 'تّ']
+    ],
+    'ث' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ج' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ح' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'خ' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'د' => [
+        'before' => true,
+        'after' => false,
+        'variations' => []
+    ],
+    'ذ' => [
+        'before' => true,
+        'after' => false,
+        'variations' => []
+    ],
+    'ر' => [
+        'before' => true,
+        'after' => false,
+        'variations' => []
+    ],
+    'ز' => [
+        'before' => true,
+        'after' => false,
+        'variations' => []
+    ],
+    'ط' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ظ' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ك' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ق' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'س' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ش' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ص' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ض' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ع' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'غ' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ف' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ل' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'م' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ن' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'و' => [
+        'before' => true,
+        'after' => false,
+        'variations' => []
+    ],
+    'ه' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+    'ي' => [
+        'before' => true,
+        'after' => true,
+        'variations' => ['يَ', 'يِ', 'يُ', 'يْ', 'يّ', 'ى', 'ئ', 'ىَ', 'ىٰ']
+    ],
+    'ء' => [
+        'before' => true,
+        'after' => true,
+        'variations' => []
+    ],
+
+]
