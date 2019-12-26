16875ef81f055ed06d5d607a52f2f66286f0d308
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2019-12-26 14:15:23 +0400

    Add example to test #4

--- examples/tajweed.php
+++ examples/tajweed.php
@@ -1,14 +1,14 @@
 <?php
 require_once('../vendor/autoload.php');
 
-$json = json_decode(file_get_contents('../data/tajweed2.json'));
+$json = json_decode(file_get_contents('../data/tajweed4.json'));
 $text = $json->data->text;
 
 $parser = new AlQuranCloud\Tools\Parser\Tajweed();
 ?>
 <link href="https://alquran.cloud/public/css/font-all.css" rel="stylesheet">
 <link href="css/tajweed.css" rel="stylesheet">
-<div class="font-uthmani" style="direction: rtl">
+<div class="font-uthmani" style="direction: rtl; font-size: 200%;">
 <?=$parser->parse($text);?>
 </div>
 
